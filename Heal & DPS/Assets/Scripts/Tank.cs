7fb7435549baa99b0ffb5be8847b22a9ee98cb9e
Caniheal <37191567+Caniheal@users.noreply.github.com>
2020-03-27 18:32:06 -0400

    Uploading project to github

---
+++ Heal & DPS/Assets/Scripts/Tank.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Tank : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public AudioSource healSound;
+
+    public Health healthBar;
+  //  float startTime = Time.time;
+//    float curTime = Time.time - startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
+        InvokeRepeating("TakeDamage", 2f, 2f);
+        
+    }
+    void TakeDamage()
+    {
+        currentHealth -= 20;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public void Heal()
+    {
+        currentHealth += 5;
+
+        healthBar.SetHealth(currentHealth);
+
+        healSound.Play();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+
+    void Update()
+    {
+        if(currentHealth <= 0)
+        {
+            SceneManager.LoadScene("Game 1");
+        }
+    }
+}
